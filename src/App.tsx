faaac578edbe5b3a49f08ece172ac7917de6c409
nicole <nicoleboliveira14@gmail.com>
2025-05-19 14:24:35 -0300

    test: visualizando comportamento do componente button do shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
 export function App() {
-  return <h1>Hello word</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button> Enviar </Button>
+    </div>
+  );
 }
